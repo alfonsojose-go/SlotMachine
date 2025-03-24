405b7ff3312edea38a02f78a3fb1be51c7cbaab9
alfonsojose-go <alfonsojose.go@rdpolytech.ca>
2025-03-24 16:12:57 -0600

    Created initial program

---
+++ SlotMachin.java
@@ -0,0 +1,9 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class SlotMachin{
+    public static void main(String[] args) {
+        //Put code here
+    }
+}
